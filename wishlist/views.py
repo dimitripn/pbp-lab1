8296e3c5279f3cb2f24ba6193c206b50fcc7b501
Dimitri Prima Nugraha <dimitriprimanugraha@gmail.com>
2022-09-15 13:24:18 +0700

    Finishing lab2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import BarangWishlist
 
 # Create your views here.
@@ -9,4 +11,19 @@ def show_wishlist(request):
         'nama': 'Dimitri Prima Nugraha'
     }
     return render(request, "wishlist.html", context)
-    
+
+def show_wishlist_XML(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_wishlist_JSON(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
