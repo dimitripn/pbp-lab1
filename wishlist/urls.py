32cdf8497f98b78616fa7676b68699a657cfe213
Dimitri Prima Nugraha <dimitriprimanugraha@gmail.com>
2022-09-22 10:54:22 +0700

    Finishing Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import show_wishlist_XML
 from wishlist.views import show_wishlist_JSON
 from wishlist.views import show_json_by_id
 from wishlist.views import show_xml_by_id
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', show_wishlist_JSON, name='show_wishlist_JSON'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
